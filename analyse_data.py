31c7ada55ad55fcf0c95589be24e07963e6a3585
João Torres <joao.torres@cmore.pt>
2023-05-24 13:15:20 +0100

    update analyse_data.py - analyse sentiment

--- analyse_data.py
+++ analyse_data.py
@@ -9,6 +9,7 @@ from bson.json_util import dumps
 
 from get_client_info import get_analysis
 from kw_extraction_api import clean_kw, get_keywords, agg_kw_daily
+from sentiment_analysis_api import ml_sent, get_sentiment, agg_sentiment_daily
 
 
 mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
@@ -70,7 +71,7 @@ def post_kw(tenant, collection):
     
     response = requests.post(kw_url, headers=headers, data=data)
     
-    #print(response.status_code)
+    print(response.status_code)
     #print(response.json())
 
 
@@ -94,11 +95,53 @@ def post_kw_daily(tenant, collection):
     response = requests.post(kw_daily_url, headers=headers, data=data)
 
     print(response.status_code)
-    print(response.json())
+    #print(response.json())
 
 
 sent_url = "https://esg-maturity.com/api/v1/reputational/sentiments"
+
+def post_sent(tenant, collection):
+
+    headers = {'Accept': 'application/json', 
+               'Content-Type': 'application/json', 
+               'Authorization': 'Bearer 2|IIvhcPW0VLmm11NXAuEVOxQMI1GLdyJ8cUntGzBB', 
+               'X-Tenant': tenant}
+    
+    cursor = collection.find(my_query, projection={'_id': False})
+    data = json.dumps({"ainfo_id": analysis[0], "data": dumps(cursor), "extracted_at": today})
+    # should I define extracted_at or is it automatically set?
+    
+    response = requests.post(sent_url, headers=headers, data=data)
+    
+    print(response.status_code)
+    #print(response.json())
+
+
 sent_daily_url = "https://esg-maturity.com/api/v1/reputational/sentiments-daily"
+
+def post_sent_daily(tenant, collection):
+
+    headers = {'Accept': 'application/json', 
+               'Content-Type': 'application/json', 
+               'Authorization': 'Bearer 2|IIvhcPW0VLmm11NXAuEVOxQMI1GLdyJ8cUntGzBB', 
+               'X-Tenant': tenant}
+    
+    cursor = collection.find(my_query, projection={'_id': False, 
+                                                        'year': False, 
+                                                        'month': False, 
+                                                        'week_of_year': False})
+    year = json.loads(dumps(collection.find(my_query)[0]))['year']
+    month = json.loads(dumps(collection.find(my_query)[0]))['month']
+    week = json.loads(dumps(collection.find(my_query)[0]))['week_of_year']
+    
+    data = json.dumps({"ainfo_id": analysis[0], "data": json.dumps(json.loads(dumps(cursor))[0]), 
+                        "year": year, "month": month, "week_of_year": week, "extracted_at": today})
+    
+    response = requests.post(sent_daily_url, headers=headers, data=data)
+
+    print(response.status_code)
+    print(response.json())
+
 
 emo_url = "https://esg-maturity.com/api/v1/reputational/emotions"
 emo_daily_url = "https://esg-maturity.com/api/v1/reputational/emotions-daily"
@@ -123,8 +166,9 @@ for tenant in analysis_per_tenant.keys():
         df_data = load_data(tenant)
 
         df = load_tweets(df_data)
-
+        """
         ### EXTRACT KEYWORDS ###
+        print("EXTRACTING KEYWORDS...")
         kw = clean_kw(df)
         get_keywords(db, kw)
 
@@ -136,11 +180,24 @@ for tenant in analysis_per_tenant.keys():
 
         kw_daily = db['kw_daily'] # collection kw_daily
         post_kw_daily(tenant, kw_daily)
-
+        print("KEYWORD EXTRACTION COMPLETED!")
+        """
         ### SENTIMENT ANALYSIS ###
+        print("RUNNING SENTIMENT ANALYSIS...")
+        ml_sent_results = ml_sent(df)
+        get_sentiment(db, ml_sent_results)
+
+        sentiment = db['sentiment'] # collection sentiment
+        post_sent(tenant, sentiment)
+
+        ### SENTIMENT DAILY ###
+        agg_sentiment_daily(db, today)
+
+        sentiment_daily = db['sentiment_daily'] # collection sentiment_daily
+        post_sent_daily(tenant, sentiment_daily)
+
+        print("SENTIMENT ANALYSIS COMPLETED!")
         
-
-
 
 if __name__ == '__main__':
 
