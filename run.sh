24cde28376b7b2aad3d1385343b8c947a3781581
lmmelo16 <joao.melo.correia@gmail.com>
2023-10-09 14:21:27 +0100

    Added /app path to run.sh to be able to run it on container

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #source venv/bin/activate
 (
-cd scrapers/all || exit
+cd /app/scrapers/all || exit
 scrapy crawl all_scrapers -O data.json -s LOG_ENABLED=0
 )
 python3 reputational_analysi.py
